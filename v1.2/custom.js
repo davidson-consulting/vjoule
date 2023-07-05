1d1e3b5fdcfeadb66873e32afae1686846b62c14
Emile Cadorel <ecadorel@gmail.com>
2023-07-05 14:43:10 +0200

    version 1.2

--- v1.2/custom.js
+++ v1.2/custom.js
@@ -2,21 +2,29 @@ $( document ).ready(function() {
     var menu = document.getElementById ("menu-bar");
     var right_buttons = menu.querySelector (".right-buttons");
 
+    var github_url = document.createElement ("a");
+    github_url.setAttribute ("id", "github-url");
+    github_url.classList.add ("fa");
+    github_url.classList.add ("fa-github");
+    github_url.setAttribute ("href", "https://github.com/davidson-consulting/vjoule/");    
+    
     var print_button = right_buttons.firstElementChild;
     var version_button = document.createElement ("i");
     version_button.setAttribute ("id", "version-toggle");
     version_button.classList.add ("fa")
 
     var version_name = document.createElement ("span");
-    version_name.textContent = "v0.2";
+    version_name.textContent = "v1.0";
 
     version_button.appendChild (version_name);
     
     version_button.setAttribute ("title", "change version");
 
-    right_buttons.removeChild (print_button);
+    right_buttons.textContent = "";
+
     right_buttons.appendChild (version_button);
-    right_buttons.appendChild (print_button);
+    right_buttons.appendChild (github_url);
+
     
     $("#version-toggle").click(function(){
         if($('.version-popup').length) {
@@ -26,7 +34,6 @@ $( document ).ready(function() {
 	        .append($('<div class="version" id="v1.2">v1.2</div>'))
 	    	.append($('<div class="version" id="v1.0">v1.0</div>'))
                 .append($('<div class="version" id="v0.2">v0.2</div>'));
-
 
             popup.insertAfter(this);
 
